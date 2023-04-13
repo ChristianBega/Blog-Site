0cab30f6860178241d37f525483ce87dcf291b97
Chris B <cbega1227@gmail.com>
2023-04-13 15:11:14 -0400

    Setup server.js file with boilerplate code.

---
+++ server/server.js
@@ -0,0 +1,66 @@
+// Set up server file :  
+// a. express server
+// b. 
+
+
+// require dotenv
+require("dotenv").config();
+// require express
+const express = require("express");
+// require apollo server
+const { ApolloServer } = require("apollo-server-express");
+// require path 
+const path = require("path");
+
+// require in auth middle ware
+// const { authMiddleware } = require("./utils/auth");
+
+// require in typeDefs and resolvers
+const { typeDefs, resolvers } = require("./schemas");
+
+// require in connection to db
+const db = require("./config/connection");
+
+// Setting up PORT - use PORT from .env or 3001
+const PORT = process.env.PORT || 3001;
+// setting express = app
+const app = express();
+
+// setting server to use ApolloServer - 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  // context: authMiddleware,
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port http://localhost:${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
+
+startApolloServer(typeDefs, resolvers);
