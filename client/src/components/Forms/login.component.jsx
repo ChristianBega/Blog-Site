81cbcbd8d0e869d25620d83305ee4c83d39c28fb
Chris B <cbega1227@gmail.com>
2023-04-17 22:34:02 -0400

    Created login form.

--- client/src/components/Forms/login.component.jsx
+++ client/src/components/Forms/login.component.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function SignUp() {
+export default function Login() {
   return (
-    <section id="signup-hero" className="hero min-h-screen bg-base-200">
+    <section id="login-hero" className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Sign up!</h1>
+          <h1 className="text-5xl font-bold">Login!</h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
             id nisi.
@@ -13,12 +13,6 @@ export default function SignUp() {
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <form className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">UserName</span>
-              </label>
-              <input type="text" placeholder="username" className="input input-bordered" />
-            </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -31,13 +25,13 @@ export default function SignUp() {
               </label>
               <input type="text" placeholder="password" className="input input-bordered" />
               <label className="label">
-                <a href="/login" className="label-text-alt link link-hover">
-                  Already have an account?
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
                 </a>
               </label>
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Sign up</button>
+              <button className="btn btn-primary">Login</button>
             </div>
           </form>
         </div>
--- client/src/pages/login.page.jsx
+++ client/src/pages/login.page.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Login from "../components/Forms/login.component";
 
 export default function LoginPage() {
   return (
-    <section id="login-page" className="container min-h-screen mx-auto | flex justify-center items-center ">
-      <div>loginPage</div>
-    </section>
+    // <section id="login-page" className="container min-h-screen mx-auto | flex justify-center items-center ">
+    // </section>
+    <Login />
   );
 }
