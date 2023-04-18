dbca67cfef272222caff1a81f9306956190abbfc
Chris B <cbega1227@gmail.com>
2023-04-17 20:55:39 -0400

    Installed dasiy UI tailwind component library. Created navbar.

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,20 @@ import LandingPage from "./pages/landing.page";
 import SignUpPage from "./pages/signUp.page";
 import LoginPage from "./pages/login.page";
 
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./components/navigation/navigation.component";
 
 function App() {
   return (
-    <Router>
+    <>
+      <Navigation />
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
         <Route path="/home" element={<HomePage />}></Route>
         <Route path="/signUp" element={<SignUpPage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
       </Routes>
-    </Router>
+    </>
   );
 }
 
