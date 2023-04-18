345724fd33483a5e5558784b08debb755e855b15
Chris B <cbega1227@gmail.com>
2023-04-17 21:51:04 -0400

    Created sign up form.

--- client/src/components/navigation/navigation.component.jsx
+++ client/src/components/navigation/navigation.component.jsx
@@ -8,16 +8,12 @@ import { FiUser, FiLogIn, FiLogOut, FiUserPlus } from "react-icons/fi";
 // FiLogOut;
 export default function Navigation() {
   return (
-    <nav className="sticky top-0 w-full, h-20 flex justify-between items-center px-8 text-white bg-zinc-900/50 backdrop-blur-sm">
+    <nav className="sticky top-0 w-full, h-20 flex justify-between items-center px-8 text-white bg-zinc-900/50 backdrop-blur-sm z-50">
       {/* <button>LOGO</button> */}
       <a href="/">
         <img className="max-h-12 rounded-full" src={Logo} alt="Brain lightbulb logo" />
       </a>
       <h1 className="text-3xl text-[#edeaea]">Coding Circle</h1>
-      {/* <button></button> */}
-      {/* <button className="btn rounded-full">
-        <FiUser size={16} />
-      </button> */}
       <div className="dropdown dropdown-end">
         <label tabIndex={0} className="btn m-1">
           <FiUser size={16} />
@@ -25,8 +21,8 @@ export default function Navigation() {
         <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-content rounded-box w-52 | mt-5">
           <li>
             <button>
-              {/* If logged in show logout  */}
-              {/* If not logged in show login  */}
+              {/* If logged in show logout icon  */}
+              {/* If not logged in show login icon  */}
               <FiLogIn />
               {/* <FiLogOut /> */}
               <Link to="/login">Login</Link>
