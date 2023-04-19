ebb032f6d4d28974e4eff481cb5f9706c112b7c8
Chris B <cbega1227@gmail.com>
2023-04-19 00:55:19 -0400

    Created basic hero landing page, uploaded herobg image. Added condtional rendering to change path on logo based on user log in.

--- client/src/components/navigation/navigation.component.jsx
+++ client/src/components/navigation/navigation.component.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 // Images
 import Logo from "../../assets/logo.jpeg";
 // Icons
-import { FiUser, FiLogIn, FiLogOut, FiUserPlus } from "react-icons/fi";
+import { FiUser, FiLogIn, FiLogOut, FiUserPlus, FiHome } from "react-icons/fi";
 // FiLogIn;
 // FiLogOut;
 export default function Navigation() {
@@ -29,7 +29,7 @@ export default function Navigation() {
   return (
     <nav className="sticky top-0 w-full, h-20 flex justify-between items-center px-8 text-white bg-zinc-900/50 backdrop-blur-sm z-50">
       {/* <button>LOGO</button> */}
-      <Link to="/">
+      <Link to={Auth.loggedIn() ? "/home" : "/"}>
         <img className="max-h-12 rounded-full" src={Logo} alt="Brain lightbulb logo" />
       </Link>
       <h1 className="text-3xl text-[#edeaea]">Coding Circle</h1>
@@ -39,12 +39,20 @@ export default function Navigation() {
         </label>
         <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-content rounded-box w-52 | mt-5">
           {Auth.loggedIn() ? (
-            <li>
-              <button onClick={logout}>
-                <FiLogOut />
-                <Link to="/">Log Out</Link>
-              </button>
-            </li>
+            <>
+              <li>
+                <button onClick={handleClick}>
+                  <FiHome />
+                  <Link to="/home">Home</Link>
+                </button>
+              </li>
+              <li>
+                <button onClick={logout}>
+                  <FiLogOut />
+                  <Link to="/">Log Out</Link>
+                </button>
+              </li>
+            </>
           ) : (
             <>
               <li>
--- client/src/pages/landing.page.jsx
+++ client/src/pages/landing.page.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import HeroBgImg from "../assets/heroImage.jpg";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function LandingPage() {
   return (
-    <section id="landing-page" className="container min-h-screen mx-auto | flex justify-center items-center ">
-      <h1 className="text-3xl font-bold underline">landingPage</h1>
-    </section>
+    // <section id="landing-page" className="container min-h-screen mx-auto | flex justify-center items-center ">
+    // </section>
+    /* <h1 className="text-3xl font-bold underline">landingPage</h1> */
+    <div className="hero min-h-screen" style={{ backgroundImage: `url(${HeroBgImg})` }}>
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+            id nisi.
+          </p>
+          <Link to="/signup">
+            <button className="btn btn-primary">Get Started</button>
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
