c7bd20c1a4d7c85642e36094123bbcff885c5445
Chris B <cbega1227@gmail.com>
2023-04-19 00:24:49 -0400

    Created queries for getting single user, all users, & blogPosts. Still troubleshooting queryMe for getting user context.

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,41 @@
 import { gql } from "@apollo/client";
+
+export const QUERY_PROFILES = gql`
+  query Users {
+    Users {
+      email
+      _id
+    }
+  }
+`;
+
+export const QUERY_SINGLE_PROFILE = gql`
+  query Users($userId: ID!) {
+    User(userId: $userId) {
+      _id
+      email
+      username
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query Query {
+    me {
+      email
+      username
+    }
+  }
+`;
+
+export const QUERY_BLOG_POSTS = gql`
+  query Query {
+    BlogPost {
+      blogPost
+      comments {
+        commentText
+        createdAt
+      }
+    }
+  }
+`;
