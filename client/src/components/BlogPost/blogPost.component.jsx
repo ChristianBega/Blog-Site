0b9625a73446ebace739b5191ca2ad88a9703423
Chris B <cbega1227@gmail.com>
2023-04-26 16:50:48 -0400

    added styling to home page, blog page, and blog post components.

--- client/src/components/BlogPost/blogPost.component.jsx
+++ client/src/components/BlogPost/blogPost.component.jsx
@@ -7,33 +7,33 @@ import { Link } from "react-router-dom";
 import StaticImg from "../../assets/staticProfileImg.jpg";
 export default function BlogPost({ blogData }) {
   return (
-    // <article>
-    <div className="card w-96 bg-base-200 shadow-xl">
-      <div className="card-body">
-        {/* Card Header */}
-        <div className="card-header | flex gap-4 items-center  ">
-          <div className="avatar">
-            <div className="w-10 rounded-full">
-              <img src={StaticImg} />
+    <article>
+      <div className="card w-96 bg-base-200 shadow-xl">
+        <div className="card-body">
+          {/* Card Header */}
+          <div className="card-header | flex gap-4 items-center  ">
+            <div className="avatar">
+              <div className="w-10 rounded-full">
+                <img src={StaticImg} />
+              </div>
             </div>
+            <h2 className="">{blogData.creator}</h2>
           </div>
-          <h2 className="">{blogData.creator}</h2>
-        </div>
-        <Link to={`/blogPage/:${blogData._id}`} state={{ currentBlogId: blogData._id }}>
-          {/* Card Description/Title */}
-          <p className="card-title | my-3">{blogData.blogTitle}</p>
-        </Link>
+          <Link to={`/blogPage/:${blogData._id}`} state={{ currentBlogId: blogData._id }}>
+            {/* Card Description/Title */}
+            <p className="card-title | my-3">{blogData.blogTitle}</p>
+          </Link>
 
-        <div className="card-actions justify-between items-center">
-          {/* Card timeStamp */}
-          <small>{blogData.createdAt}</small>
-          {/* <button className="btn btn-primary | flex gap-2">
+          <div className="card-actions justify-between items-center">
+            {/* Card timeStamp */}
+            <small>{blogData.createdAt}</small>
+            {/* <button className="btn btn-primary | flex gap-2">
             <FiSave />
             save
           </button> */}
+          </div>
         </div>
       </div>
-    </div>
-    // </article>
+    </article>
   );
 }
